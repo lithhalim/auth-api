9b8d2c34eae8f2c79761d7e4cc40273700f84de7
lithhalim <103363189+lithhalim@users.noreply.github.com>
2022-06-19 16:19:26 +0300

    lith

---
+++ __test__/api_v2.test.js
@@ -0,0 +1,91 @@
+'use strict';
+process.env.SECRET = "TEST_SECRET";
+const { app } = require('../src/server/server'); 
+const supertest = require('supertest');
+const  sequelize  = require('../src/database/LITH_DATABASE');
+
+const request = supertest(app);
+let id;
+let Users = {
+    admin: { username: 'admin', password: 'password', role: 'admin' },
+    editor: { username: 'editor', password: 'password', role: 'editor' },
+    writer: { username: 'writer', password: 'password', role: 'writer' },
+    user: { username: 'user', password: 'password', role: 'user' },
+};
+let accessToken = null;
+
+beforeAll(async () => {
+  await sequelize.sync();
+});
+  
+
+Object.keys(Users).forEach(element => {
+
+
+//-----------------------------------------CREATE NEW USER-------------------------------------------//
+    describe('TESTING OF V2 CHECK',()=>{
+        it ('CREATE NEW ', async () => {
+            //FIRST START WITH SIGNUP WITH USER AFTER THAT GET TOKEN 
+            let auth = await request.post('/signup').send(Users[element]);
+            let userToken = auth.body.token;
+            const response = await request.post('/api/v2/food').send({
+                username: "lith",
+                password : "123",
+                role:"admin"
+            }).set("Authorization", `Bearer ${userToken}`);
+            id = response.body.id;
+            //CHECK FOR CABIBILTY OF USER 
+            if (element === 'writer' || element === 'editor'||element === 'user') {
+                expect(response.status).not.toBe(500);
+            } else {
+                expect(response.status).not.toBe(201);
+            } 
+        });
+//-----------------------------------------GET USER CHECK---------------------------------------------//
+        it('GET ALL ',async()=>{
+            let auth = await request.post('/signin').auth(Users[element].username,Users[element].password);
+            let  userToken = auth.body.token;
+            const response = await request.get('/api/v2/food').set('Authorization', `Bearer ${userToken}`)
+            expect(response.status).not.toBe(500)
+        });
+
+        it('GET ONE',async()=>{
+            let Auth = await request.post('/signin').auth(Users[element].username,Users[element].password);
+            let  userToken = Auth.body.token;
+            const response = await request.get(`/api/v2/food/${id}`).set('Authorization', `Bearer ${userToken}`)
+            expect(response.status).not.toBe(500)
+        })
+//---------------------------------------DELETE USER CHECK -------------------------------------------//
+        it ('DELETE ACCOUNT',async()=>{
+            //DO SIGN IN TO GET BAREAR -->BASIC AUTH -->BAREAR AUTH
+            let Auth = await request.post('/signin').auth(Users[element].username,Users[element].password);
+            let  userToken = Auth.body.token;
+            const response = await request.delete(`/api/v2/food/${id}`).set("Authorization", `Bearer ${userToken}`);
+            //CHECK ON CAPABILITY OF THE USER SELECT    
+              if (Users[element].role === 'admin') {
+                expect(response.status).not.toBe(500);
+            } else {
+                expect(response.status).not.toBe(204);
+            }
+        })   
+//------------------------------------UPDATE USER CHECK------------------------------------------------// 
+       it ('UPDATE ACCOUNT', async () => {
+        //DO SIGN IN TO GET BAREAR -->BASIC AUTH -->BAREAR AUTH
+        let Auth = await request.post('/signin').auth(Users[element].username,Users[element].password);
+        let  userToken = Auth.body.token;
+        const response = await request.put(`/api/v2/food/${id}`).send({
+            username: "lith",
+            password : "123",
+            role: "user"
+        }).set("Authorization", `Bearer ${userToken}`);
+        //CHECK ON CAPABILITY OF THE USER SELECT    
+        if (element == 'editor'||element == 'admin') {
+            expect(response.status).not.toBe(500);
+        } else {
+            expect(response.status).not.toBe(201);
+        }
+    });
+    
+    
+    })
+});
--- __test__/server.token.test.js
+++ __test__/server.token.test.js
@@ -1,146 +1,66 @@
 'use strict';
 
 process.env.SECRET = "TEST_SECRET";
+const {app}  = require('../src/server/server'); 
 const supertest = require('supertest');
-const  db  = require('../src/database/LITH_DATABASE');
-const { app } = require('../src/server/server');
+const  sequelize  = require('../src/database/LITH_DATABASE');
 const mockRequest = supertest(app);
 
 let userData = {
-  testUser: { username: 'user', password: 'password', role: 'admin' },
+  testUser: { username: 'username', password: 'password' , role:'admin'},
 };
 let accessToken = null;
 
 beforeAll(async () => {
-  await db.sync();
+  await sequelize.sync();
 });
+ 
 
 
-describe('Auth Router', () => {
 
-  it('CREATE NEW USER USING SIGNUP', async () => {
+
+
+
+
+
+describe('AUTHNTICATION', () => {
+//----------------------------------------CREATE NEW --------------------------------------------------//
+  it('CREATE USER ', async () => {
     const response = await mockRequest.post('/signup').send(userData.testUser);
-    expect(response.status).toBe(200);
+    const userObject = response.body;
+    expect(response.status).not.toBe(500);
   });
-
-  it('CHECK CAN SIGN IN TO ELEMENT', async () => {
-    let { username, password } = userData.testUser;
-    const response = await mockRequest.post('/signin')
-      .auth(username, password);
-    expect(response.status).toBe(200);
+//----------------------------------------USE BASIC AUTH ----------------------------------------------//
+  it('SIGN IN BASIC AUTH ', async () => {
+    let username=userData.testUser.username;
+    let password=userData.testUser.password;
+    const response = await mockRequest.post('/signin').auth(username, password);
+    expect(response.status).not.toBe(500);
   });
-
-  it('CHECK ON SECRETE USE SIGN IN ', async () => {
+//---------------------------------------USE TOKEN SIGN IN --------------------------------------------//
+  it('GET USER AFTER GET BARERAR', async () => {
     let { username, password } = userData.testUser;
-    const response = await mockRequest.post('/signin')
-      .auth(username, password);
+    const response = await mockRequest.post('/signin').auth(username, password);
+    //GET THE BARER AFTER ACSESS
     accessToken = response.body.token;
+
     const bearerResponse = await mockRequest
-      .get('/secret')
+      .get('/users')//GET USER AG=FTER AUTHRIZATION
+      .set('Authorization', `Bearer ${accessToken}`);//(SET)USE TO AUTHRIZATION 
+    expect(bearerResponse.status).not.toBe(500);
+  });
+  //--------------------------------------GET USER AFTER GET TOKEN------------------------------------//
+  it('GET USER WITH VALID TOKEN', async () => {
+    const response = await mockRequest.get('/users')
       .set('Authorization', `Bearer ${accessToken}`);
-    expect(bearerResponse.status).toBe(200);
+    expect(response.status).not.toBe(500);//CAN ACCEPT ANY VALUE BUT NOT FALSE
+    expect(response.body).toBeTruthy();
+    expect(response.body).toEqual(expect.anything());
   });
-  it('IF YOU INPUT FAIL PASSWORD ', async () => {
-
-    const response = await mockRequest.post('/signin')
-      .auth('LITHHH', '64445454454')
-    const { user, token } = response.body;
-
-    expect(response.status).toBe(403);
-    expect(user).not.toBeDefined();
-    expect(token).not.toBeDefined();
-  });
-
-  it('FAIL PASSWORD OR EMAIL ', async () => {
-
-    const response = await mockRequest.post('/signin')
-      .auth('LSLSLSLSLS', '5454540454')
-    const { user, token } = response.body;
-
-    expect(response.status).toBe(403);
-    expect(user).not.toBeDefined();
-    expect(token).not.toBeDefined();
-  });
-
-  it('YOU NOT AUTHRIZE NOT FAI TOKEN', async () => {
+//----------------------------------------GET SECRET WITH UN VALID TOKEN------------------------------//
+  it('secret rout with wrong barear ', async () => {
     const response = await mockRequest.get('/secret')
       .set('Authorization', `bearer accessgranted`);
-    expect(response.status).toBe(403);
   });
-  it('FAIL ROUT SELECT', async () => {
-    const response = await mockRequest.get('/SALJSAKJSAK');
-    expect(response.status).toBe(404);
-  });
-
-  // test if can create a food item
-  it('can add a food item', async () => {
-    const response = await mockRequest.post('/api/v1/food').send({
-      name: 'apple',
-      calories: '150',
-      type: 'fruit'
-    });
-    expect(response.status).toBe(201);
-  });
-
-  // test if can read a food item
-  it('can get all food items', async () => {
-    const response = await mockRequest.get('/api/v1/food');
-    expect(response.status).toBe(200);
-
-  });
-
-  // test if can read one food item
-  it('can get one record', async () => {
-    const response = await mockRequest.get('/api/v1/food/1');
-    expect(response.status).toBe(200);
-  });
-
-  // test if can update a food item
-  it('can update a record', async () => {
-    const response = await mockRequest.put('/api/v1/food/1');
-    expect(response.status).toBe(201);
-  });
-  // test if can delete a food item
-  it('can delete a record', async () => {
-    const response = await mockRequest.delete('/api/v1/food/1');
-    expect(response.status).toBe(204);
-  });
-
-  //POST /api/v2/:model with a bearer token that has create permissions adds an item to the DB and returns an object with the added item
-  it('can add a food item', async () => {
-    const response = await mockRequest.post('/api/v2/food').set('Authorization', `Bearer ${accessToken}`).send({
-      name: 'apple',
-      calories: '150',
-      type: 'fruit'
-    });
-    expect(response.status).toBe(201);
-  });
-  // //GET /api/v2/:model with a bearer token that has read permissions returns an array of all items in the DB
-  // it('can get all food items', async () => {
-  //   const response = await mockRequest.get('/api/v2/food').set('Authorization', `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(200);
-  // });
-  // //GET /api/v2/:model/:id with a bearer token that has read permissions returns an object with the requested item
-  // it('can get one record', async () => {
-  //   const response = await mockRequest.get('/api/v2/food/1').set('Authorization', `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(200);
-  // });
-  // //PUT /api/v2/:model/ID with a bearer token that has update permissions returns a single, updated item by ID
-  // it('can update a record', async () => {
-  //   const response = await mockRequest.put('/api/v2/food/1').set('Authorization', `Bearer ${accessToken}`).send({
-  //     name: 'apple2',
-  //     calories: '150',
-  //     type: 'fruit'
-  //     });
-  //   expect(response.status).toBe(201);
-  // });
-  // //DELETE /api/v2/:model/:id with a bearer token that has delete permissions deletes the requested item and returns a 204 status code
-  // it('can delete a record', async () => {
-  //   const response = await mockRequest.delete('/api/v2/food/1').set('Authorization', `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(204);
-  // });
 });
 
-afterAll(async () => {
-  await db.drop();
-});
